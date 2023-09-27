d1d6c3279f748b8fb99257cad658765625b9f2e8
Irene <irene.muthomi@student.moringaschool.com>
2023-09-27 23:09:06 +0300

    Completed assignment.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return "This Saturday, I want to roller-skate!";
+
+  }
+ console.log(saturdayFun());
+ console.log(saturdayFun("bathe my dog!"))
+  
+
+
+const mondayWork = function(activity= "go to the office") {
+    return "This Monday, I will &{go to the office.}.";
+
+} ;
+console.log(mondayWork());
+console.log(mondayWork("work from home"))
+
+
+function wrapAdjective(door = '*') {
+    return function (name, adverb = "special") {
+      return (`You are ${door}${name}${door}!`)
+    };
+    }
